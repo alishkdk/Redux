56c6d50b23e4a7a1a222e2074ac2586fb7b1b9d2
alishkdk <kdkalish4@gmail.com>
2026-02-01 07:10:56 +0545

    updated:boxreducer

--- redux/app/store.ts
+++ redux/app/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../feature/counter/counterSlice";
-import boxReducer from "../feature/counter/box/boxSlice";
+import boxReducer from "../feature/box/boxSlice";
 
 export const store = configureStore({
   reducer: {
