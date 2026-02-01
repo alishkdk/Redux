d0cd3841acc50e0edc95cc32dda55556bfad1688
alishkdk <kdkalish4@gmail.com>
2026-02-01 07:01:59 +0545

    updated: add boxslice redux

--- redux/app/store.ts
+++ redux/app/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../feature/counter/counterSlice";
+import boxReducer from "../feature/counter/box/boxSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+        box: boxReducer,
   },
 });
